887d06af7dd2f440ae74398d4349ae3e6da6e390
nicolasdca <nicolasdavid193@gmail.com>
2023-04-26 21:36:21 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const itens = [];
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach((item) => {
+  criaElemento(item);
+});
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const nome = event.target.elements['nome'];
   const quantidade = event.target.elements['quantidade'];
 
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem('itens', JSON.stringify(itens));
+
   //   console.log(event.target.elements['nome'].value);
   //   console.log(event.target.elements['quantidade'].value);
 
-  criaElemento(nome.value, quantidade.value);
+  criaElemento(itemAtual);
 
   nome.value = '';
   quantidade.value = '';
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
   const novoItem = document.createElement('li');
   novoItem.classList.add('item');
 
   const numeroItem = document.createElement('strong');
-  numeroItem.innerHTML = quantidade;
+  numeroItem.innerHTML = item.quantidade;
 
   novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
-
-  const itemAtual = {
-    nome: nome,
-    quantidade: quantidade,
-  };
-
-  itens.push(itemAtual);
-
-  localStorage.setItem('item', JSON.stringify(itens));
 }
